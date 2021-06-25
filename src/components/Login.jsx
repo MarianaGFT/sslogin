a2774050d1c0a2a8d9d246f19549728f0576b01b
joseLuisMtzE <jmartinez50@ucol.mx>
2021-06-25 08:40:50 -0500

    funcionalidad

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LoginCss from "./Login.css";
 import { Form, Button } from "react-bootstrap";
 
 function Login() {
   const [contrasena, setContrasena] = useState("");
   const [email, setEmail] = useState("");
+  const [counterErrors, setCounterErrors] = useState(0);
+  const [countdown, setCountdown] = useState(10);
 
   const emailRegex =
     /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
@@ -16,16 +18,21 @@ function Login() {
     if (user && password === true) {
       console.log(user, password);
       alert("Contraseña correcta");
-      window.location.href = '/home';
-
-      // window.location.href = "/home";
-
+      window.location.href = "/home";
     } else {
-      alert("Escribe una contraseña correcta");
+      if (counterErrors === 2) {
+        console.log("if", counterErrors);
+        setTimeout(function () {
+          setCounterErrors(0);
+          window.location.reload();
+        }, 10000);
+      }
+      alert("⚠ Contraseña incorrecta ⚠");
       setEmail("");
       setContrasena("");
+      setCounterErrors(counterErrors + 1);
+      console.log(counterErrors);
     }
-    // console.log(contrasena,email)
   };
 
   const checkEmail = (email) => {
@@ -78,6 +85,65 @@ function Login() {
     return false;
   };
 
+  const checkMayuscula = (contrasena) => {
+    var mayuscula = false;
+
+    for (var i = 0; i < contrasena.length; i++) {
+      if (contrasena.charCodeAt(i) >= 65 && contrasena.charCodeAt(i) <= 90) {
+        mayuscula = true;
+      }
+    }
+    if (mayuscula == true) {
+      return true;
+    }
+    return false;
+  };
+
+  const checkMinuscula = (contrasena) => {
+    var minuscula = false;
+
+    for (var i = 0; i < contrasena.length; i++) {
+      if (contrasena.charCodeAt(i) >= 97 && contrasena.charCodeAt(i) <= 122) {
+        minuscula = true;
+      }
+    }
+    if (minuscula == true) {
+      return true;
+    }
+    return false;
+  };
+
+  const checkNumero = (contrasena) => {
+    var numero = false;
+
+    for (var i = 0; i < contrasena.length; i++) {
+      if (contrasena.charCodeAt(i) >= 48 && contrasena.charCodeAt(i) <= 57) {
+        numero = true;
+      }
+    }
+    if (numero == true) {
+      return true;
+    }
+    return false;
+  };
+
+  const checkChar = (contrasena) => {
+    var caracter_raro = false;
+
+    for (var i = 0; i < contrasena.length; i++) {
+      var expreg = /^[A-Z][a-z]$/;
+      if (expreg.test(contrasena)) {
+        caracter_raro = false;
+      } else {
+        caracter_raro = true;
+      }
+    }
+    if (caracter_raro == true) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <div className="login-container">
       <div className="login-box">
@@ -88,12 +154,26 @@ function Login() {
                         <a className={contrasena.length >= 8 ? "success" : null}>
                         8 carácteres como mínimo.
                       </a> */}
-            <h1 className="primary">Requisitos password</h1>
-            <a className="success">8 carácteres como mínimo.</a>
-            <a className="success">Al menos una letra minuscula.</a>
-            <a className="success">Al menos una letra mayuscula.</a>
-            <a className="success">Al menos un caracter raro.</a>
-            <a className="success">Al menos un número.</a>
+            <h1 className="primary">Requisitos de contraseña</h1>
+            <a className={contrasena.length >= 8 ? "success" : null}>
+              8 carácteres como mínimo.
+            </a>
+            <a
+              className={checkMinuscula(contrasena) === true ? "success" : null}
+            >
+              Al menos una letra minuscula.
+            </a>
+            <a
+              className={checkMayuscula(contrasena) === true ? "success" : null}
+            >
+              Al menos una letra mayuscula.
+            </a>
+            <a className={checkChar(contrasena) === true ? "success" : null}>
+              Al menos un caracter raro.
+            </a>
+            <a className={checkNumero(contrasena) === true ? "success" : null}>
+              Al menos un número.
+            </a>
           </div>
         </section>
         <section className="form-wrapper">
@@ -101,29 +181,66 @@ function Login() {
             <h2>USER LOGIN</h2>
             <Form>
               <Form.Group controlId="formBasicEmail">
-                <Form.Control
-                  value={email}
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                  type="email"
-                  placeholder="Enter email"
-                  
-                />
+                {counterErrors >= 3 && (
+                  <>
+                    <div className="alert alert-danger" role="alert">
+                      Login deshabilitado por intentos fallidos intentelo mas tarde
+                    </div>
+                    <Form.Control
+                      value={email}
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                      type="email"
+                      placeholder="Enter email"
+                      disabled
+                    />
+                  </>
+                )}
+                {counterErrors < 3 && (
+                  <>
+                    <Form.Control
+                      value={email}
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                      type="email"
+                      placeholder="Enter email"
+                    />
+                  </>
+                )}
                 <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
                 </Form.Text>
               </Form.Group>
 
               <Form.Group controlId="formBasicPassword">
-                <Form.Control
-                  value={contrasena}
-                  onChange={(e) => {
-                    setContrasena(e.target.value);
-                  }}
-                  type="password"
-                  placeholder="Password"
-                />
+                {counterErrors >= 3 && (
+                  <>
+                    <Form.Control
+                      value={contrasena}
+                      onChange={(e) => {
+                        setContrasena(e.target.value);
+                      }}
+                      type="password"
+                      placeholder="Password"
+                      disabled
+                    />
+                  </>
+                )}
+                {counterErrors < 3 && (
+                  <>
+                    <Form.Control
+                      value={contrasena}
+                      onChange={(e) => {
+                        setContrasena(e.target.value);
+                      }}
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </>
+                )}
+              
               </Form.Group>
 
               <Button variant="primary" type="submit" onClick={onSubmit}>
